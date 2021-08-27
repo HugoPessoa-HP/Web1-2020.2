0863664e3aa7e8fb310932d9f3ec6adc0d57904d
Hugo Pessoa de Souza <77171035+HugoPessoa-HP@users.noreply.github.com>
2021-08-26 23:13:55 -0300

    Delete conexaoFormularioTarefa2.php

--- conexaoFormularioTarefa2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "root";
-	$dbname = "bancoDeDados";
-	
-	//Criar a conexão
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
